bfafb8843d40c9038c5b3eb2c334865ace00b324
Louis-Idundun <louis.idundun@decagon.dev>
2024-07-15 05:19:45 -0500

    first commit

--- src/main/java/com/flexisaf/flexisafinternship/EnhancementAnnotation.java
+++ src/main/java/com/flexisaf/flexisafinternship/EnhancementAnnotation.java
@@ -1,12 +1,6 @@
 package com.flexisaf.flexisafinternship;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
-//@Retention(RetentionPolicy.RUNTIME)
-//@Target(ElementType.TYPE)
 public @interface EnhancementAnnotation {
     String id();
     String synopsis();
